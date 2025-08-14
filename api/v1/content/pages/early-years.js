46c3cf3477178bd9a72eabddee362482bd0b7457
German <germankrajcar@gmail.com>
2025-08-14 11:24:15 -0300

    add sdui component methods

--- api/v1/content/pages/early-years.js
+++ api/v1/content/pages/early-years.js
@@ -1,45 +1,26 @@
+import { navBar, navBarLink } from "../../sdui/components/navigation/navbar";
+import { title, paragraph } from "../../sdui/components/text/text"
+
 export default function handler(req, res) {
-    const firstParagraph = "Even though our journey starts in 1994, we have to first go a bit back in time. In particular to the year 1987," +
-        "year in which Argentina achieved a connection between its Ministry of Foreign Affairs and the University of Toronto." + 
-        "This was the first internation communication of the country via e-mail, using the protocol UUCP. As a side note," +
+    const firstParagraph = "Even though our journey starts in 1994, we have to first go a bit back in time. In particular to the year 1987, " +
+        "year in which Argentina achieved a connection between its Ministry of Foreign Affairs and the University of Toronto. " + 
+        "This was the first internation communication of the country via e-mail, using the protocol UUCP. As a side note, " +
         "this connection was possible by first communicating via telephone. "
     
     const response = {
         pageTitle: "Early years",
         content: [
-            {
-                type: "navbar",
-                data: {
-                    icon: "assets/logo.png",
-                    title: "The history of the Internet in Argentina",
-                    links: [
-                        {
-                            text: "Home",
-                            url: "index.html"
-                        },
-                        {
-                            text: "Mass adoption",
-                            url: "index.html"
-                        },
-                        {
-                            text: "Modern era",
-                            url: "index.html"
-                        }
-                    ]
-                }
-            },
-            {
-                type: "title",
-                data: {
-                    text: "Early years"
-                }
-            },
-            {
-                type: "paragraph",
-                data: {
-                    text: firstParagraph
-                }
-            }
+            navBar(
+                "assets/logo.png",
+                "The history of the Internet in Argentina",
+                [
+                    navBarLink("Home", "index.html"),
+                    navBarLink("Mass adoption", "index.html"),
+                    navBarLink("Modern era", "index.html")
+                ]
+            ),
+            title("Early years"),
+            paragraph(firstParagraph)
         ]
     }
 
